e841b77f7e1e27fcecb2d966fd8645c8b14a8b2f
SanTae 🥷 <sankvas00@gmail.com>
2022-07-11 18:19:52 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import emojiData from "./emoji.json"; //json file having all emoji data
 function App() {
+  const emptyArrary = emojiData; // json file imported with all emoji interpretation
+  const [emoji, setEmoji] = useState(""); // used to track current selected emoji
+  const [emojiTranslation, setemojiTranslation] = useState(
+    "emoji description will appear here"
+  ); // dynamically change the value of description for the emoji
+
+  // function to check for a description for the input emoji from the emojidata api
+
+  // **********function declarations**********************//
+  function hello(items, emoji) {
+    const value = items.find(function (list, index) {
+      return list.char === emoji;
+    });
+    let description = value;
+    return description;
+  }
+
+  function changeHandler(e) {
+    let inputEmoji = e.target.value;
+    let data = hello(emptyArrary, inputEmoji);
+
+    setemojiTranslation(data.name);
+    setEmoji(inputEmoji);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="heading">Emoji Translator</header>
+      <div className="container">
+        <input
+          // value={emoji}
+          onChange={changeHandler}
+          placeholder="enter the emoji here"
+        />
+        <div className="image">
+          <span>{emoji}</span>
+          <h3 class>{emojiTranslation}</h3>
+        </div>
+      </div>
     </div>
   );
 }
